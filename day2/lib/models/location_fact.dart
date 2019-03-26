32842fdbc7c9f15c7777f4081eef9a57368ce70b
NenadSpasic <nenad.s.spasic@gmail.com>
2019-03-26 18:46:40 +0100

    Creating models

---
+++ day2/lib/models/location_fact.dart
@@ -0,0 +1,13 @@
+class LocationFact {
+  final String title;
+  final String text;
+  LocationFact({this.title, this.text});
+}
+
+/*
+LocationFact je drugi model sa definisanim svojstvima 
+i KONSTRUKTOROM 'LocationFact' koji se ispisuje kao Lista 
+u modelu koji sam definisao kao "location.dart"
+
+
+*/
